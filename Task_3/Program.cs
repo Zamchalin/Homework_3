f39412693a1f9c00d0da4498a774f62309a30af1
Michael <zamchalin@mail.ru>
2022-11-29 13:27:31 +0300

    Added mastake

--- Task_3/Program.cs
+++ Task_3/Program.cs
@@ -1,5 +1,5 @@
 ﻿Console.Clear();
-Console.Write("Ведите число: ");
+Console.Write("Введите число: ");
 int n = Convert.ToInt32(Console.ReadLine());
 for (int i = 1; i <= n; i++)
     Console.Write($"{i * i * i} ");
