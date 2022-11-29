ff3e6cf20a350cff0634af1c618d1f60c66cede1
Michael <zamchalin@mail.ru>
2022-11-29 13:30:00 +0300

    Mistackes

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,5 +1,5 @@
 ﻿Console.Clear();
-Console.Write("Ведите пятизначное число: ");
+Console.Write("Введите пятизначное число: ");
 int n = Convert.ToInt32(Console.ReadLine());
 
 if( n < 10000 || n > 100000)
